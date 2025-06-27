d925fdcb65323db193bc9e1800f736b95ed87403
eldor9313 <akhmadjonoveldorbek@gmail.com>
2025-06-28 02:10:41 +0900

    feat: create subscribe, unsubscribe, getMemberFollowings and getMemberFollowers GraphQL APIs

--- apps/nestar-api/src/libs/enums/common.enum.ts
+++ apps/nestar-api/src/libs/enums/common.enum.ts
@@ -18,7 +18,7 @@ export enum Message {
 	ONLY_SPECIFIC_ROLES_ALLOWED = 'Allowed only for members with specific roles!',
 	NOT_ALLOWED_REQUEST = 'Not Allowed Request!',
 	PROVIDE_ALLOWED_FORMAT = 'Please provide jpg, jpeg or png images!',
-	SELF_SUBSCRIPATION_DENIED = 'Self subscription is denied!',
+	SELF_SUBSCRIPTION_DENIED = 'Self subscription is denied!',
 }
 
 export enum Direction {
