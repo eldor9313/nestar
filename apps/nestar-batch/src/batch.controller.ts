4c0675ad924b09782e6804888edaa3793b55619f
eldor9313 <akhmadjonoveldorbek@gmail.com>
2025-07-03 00:48:36 +0900

    feat: create batchRollback, batchProperties and batchAgents jobSchedulers logic
    Changes to be committed:

--- apps/nestar-batch/src/batch.controller.ts
+++ apps/nestar-batch/src/batch.controller.ts
@@ -1,12 +1,59 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { BatchService } from './batch.service';
+import { Cron, Interval, Timeout } from '@nestjs/schedule';
+import { BATCH_ROLLBACK, BATCH_TOP_AGENTS, BATCH_TOP_PROPERTIES } from './lib/config';
 
 @Controller()
 export class BatchController {
-	constructor(private readonly nestarBatchService: BatchService) {}
+	private logger: Logger = new Logger('BatchController');
 
+	constructor(private readonly BatchService: BatchService) {}
+
+	@Timeout(1000) // Server ishga tushganda
+	handleTimeout() {
+		this.logger.debug('BATCH SERVER READY!');
+	}
+	@Cron('00 * * * * *', { name: BATCH_ROLLBACK })
+	public async batchRollback() {
+		try {
+			this.logger['context'] = BATCH_ROLLBACK;
+			this.logger.debug('EXECUTED!');
+			await this.BatchService.batchRollback();
+		} catch (err) {
+			this.logger.error(err);
+		}
+	}
+
+	@Cron('20 * * * * *', { name: BATCH_TOP_PROPERTIES })
+	public async batchProperties() {
+		try {
+			this.logger['context'] = BATCH_TOP_PROPERTIES;
+			this.logger.debug('EXECUTED!');
+			await this.BatchService.batchProperties();
+		} catch (err) {
+			this.logger.error(err);
+		}
+	}
+
+	@Cron('40 * * * * *', { name: BATCH_TOP_AGENTS })
+	public async batchAgents() {
+		try {
+			this.logger['context'] = BATCH_TOP_AGENTS;
+			this.logger.debug('EXECUTED!');
+			await this.BatchService.batchAgents();
+		} catch (err) {
+			this.logger.error(err);
+		}
+	}
+
+	/*
+	@Interval(1000) // Har sekundda
+	handleinterval() {
+		this.logger.debug('INTERVAL TEST');
+	}
+    */
 	@Get()
 	getHello(): string {
-		return this.nestarBatchService.getHello();
+		return this.BatchService.getHello();
 	}
 }
