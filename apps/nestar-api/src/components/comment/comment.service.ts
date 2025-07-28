b843e67e37690803525312a0fed036712176906a
eldor9313 <akhmadjonoveldorbek@gmail.com>
2025-07-28 23:40:12 +0900

    feat: modify getAllMembersByAdmin logic

--- apps/nestar-api/src/components/comment/comment.service.ts
+++ apps/nestar-api/src/components/comment/comment.service.ts
@@ -64,17 +64,19 @@ export class CommentService {
 	// updateComment logic
 	public async updateComment(memberId: ObjectId, input: CommentUpdate): Promise<Comment> {
 		const { _id } = input;
-		const result = await this.commentModel.findOneAndUpdate(
-			{
-				_id: _id,
-				memberId: memberId,
-				commentStatus: CommentStatus.ACTIVE,
-			},
-			input,
-			{
-				new: true,
-			},
-		);
+		const result = await this.commentModel
+			.findOneAndUpdate(
+				{
+					_id: _id,
+					memberId: memberId,
+					commentStatus: CommentStatus.ACTIVE,
+				},
+				input,
+				{
+					new: true,
+				},
+			)
+			.exec();
 		if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
 		return result;
 	}
@@ -85,22 +87,24 @@ export class CommentService {
 		const match: T = { commentRefId: commentRefId, commentStatus: CommentStatus.ACTIVE };
 		const sort: T = { [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC };
 
-		const result: Comments[] = await this.commentModel.aggregate([
-			{ $match: match },
-			{ $sort: sort },
-			{
-				$facet: {
-					list: [
-						{ $skip: (input.page - 1) * input.limit },
-						{ $limit: input.limit },
-						// meLiked
-						lookupMember,
-						{ $unwind: '$memberData' },
-					],
-					metaCounter: [{ $count: 'total' }],
+		const result: Comments[] = await this.commentModel
+			.aggregate([
+				{ $match: match },
+				{ $sort: sort },
+				{
+					$facet: {
+						list: [
+							{ $skip: (input.page - 1) * input.limit },
+							{ $limit: input.limit },
+							// meLiked
+							lookupMember,
+							{ $unwind: '$memberData' },
+						],
+						metaCounter: [{ $count: 'total' }],
+					},
 				},
-			},
-		]);
+			])
+			.exec();
 		if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
 
 		return result[0];
